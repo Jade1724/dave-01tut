6c79702e211b85f8f4c860472c17098aa503c122
Haruka Ichinose <hic21@uclive.ac.nz>
2022-05-08 21:55:17 +1200

    Click event handling

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,29 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log('You clicked it');
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  }
+
+  const handleDoubleClick = (event) => {
+    console.log(event);
+  }
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleDoubleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2('Dave')}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
