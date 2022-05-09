b759cc97c14c5857bd1f328abb7545e4a9808fa5
Haruka Ichinose <hic21@uclive.ac.nz>
2022-05-10 02:52:20 +1200

    Decomposed the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import {useState} from 'react';
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "Cream cheese",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Yoghurt",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Fruit",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
 
   return (
     <div className="App">
       <Header />
-      <Content />
-      <Footer />
+      <Content
+        items={items}
+        setItems={setItems}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length}/>
     </div>
   );
 }
